6f31cf041634115a40987bd0597d6d73ca557724
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-22 06:32:02 -0700

    Create activityTypeManagement.js
    Function to add a new activity type

---
+++ activityTypeManagement.js
@@ -0,0 +1,14 @@
+// Function to add a new activity type
+export function addActivityType(activityType) {
+  // Add activity type to the database or data store
+}
+
+// Function to delete an activity type
+export function deleteActivityType(activityTypeId) {
+  // Delete activity type from the database or data store
+}
+
+// Function to get all activity types
+export function getActivityTypes() {
+  // Retrieve activity types from the database or data store
+}
