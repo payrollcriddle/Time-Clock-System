affc6862cee02f529467e210507d04a198b099f8
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-23 20:27:40 -0700

    Create california.js
    California meal period

---
+++ mealPeriodPolicies/california.js
@@ -0,0 +1,7 @@
+export const californiaPolicy = {
+  mealPeriodDuration: 30,
+  mealPeriodThreshold: 5,
+  secondMealPeriodThreshold: 10,
+  maxHoursWithoutSecondMealPeriod: 12,
+  waivable: true,
+};
