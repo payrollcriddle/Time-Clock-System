0ee993c976d37a4c7c366e4f0986be8d5ca6f355
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-23 19:36:30 -0700

    Create oregon.js
    Oregon State Regulations

---
+++ stateRegulations/oregon.js
@@ -0,0 +1,10 @@
+export function oregonRegulations(weeklyHours) {
+  let regularHours = Math.min(40, weeklyHours);
+  let overtimeHours = 0;
+
+  if (weeklyHours > 40) {
+    overtimeHours = weeklyHours - 40;
+  }
+
+  return { regularHours, overtimeHours };
+}
