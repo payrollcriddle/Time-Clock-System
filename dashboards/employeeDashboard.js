23f763dcb2ea1b2bf2505c25330481213d29761b
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-28 20:29:00 -0700

    Update employeeDashboard.js

--- dashboards/employeeDashboard.js
+++ dashboards/employeeDashboard.js
@@ -5,9 +5,9 @@ import { getActivityTypes } from '../management/activityTypeManagement.js';
 import { getJobs } from '../management/jobManagement.js';
 import { clockIn, clockOut, startMeal, endMeal, getTimecard, submitTimecard, submitLeaveHours, updateTimecard } from '../timecard.js';
 import { calculateHours } from '../hoursCalculation.js';
-import { getPayPeriodStartDate, getPayPeriodEndDate, getNextPayDate } from '../employeeDashboardModules/timesheetFunctions.js';
-import { Calendar } from '../employeeDashboardModules/calendarFunctions.js';
-import { updateCurrentTime, updateDailyHoursDisplay, updateWeeklyHoursDisplay, updateTimeClockDisplay } from '../employeeDashboardModules/displayFunctions.js';
+import { getPayPeriodStartDate, getPayPeriodEndDate, getNextPayDate } from './employeeDashboardModules/timesheetFunctions.js';
+import { Calendar } from './employeeDashboardModules/calendarFunctions.js';
+import { updateCurrentTime, updateDailyHoursDisplay, updateWeeklyHoursDisplay, updateTimeClockDisplay } from './employeeDashboardModules/displayFunctions.js';
 
 // ...
 
