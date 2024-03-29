b4db705894d8121a7e187666be902337c005e2d3
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-28 21:02:15 -0700

    Update mealPeriodPolicies.js

--- mealPeriodPolicies.js
+++ mealPeriodPolicies.js
@@ -1,11 +1,11 @@
-import { californiaPolicy } from '../mealPeriodPolicies/california.js';
-import { montanaPolicy } from '../mealPeriodPolicies/montana.js';
-import { nevadaPolicy } from '../mealPeriodPolicies/nevada.js';
-import { oregonPolicy } from '../mealPeriodPolicies/oregon.js';
-import { washingtonPolicy } from '../mealPeriodPolicies/washington.js';
-import { coloradoPolicy } from '../mealPeriodPolicies/colorado.js';
-import { idahoPolicy } from '../mealPeriodPolicies/idaho.js';
-import { wyomingPolicy } from '../mealPeriodPolicies/wyoming.js';
+import { californiaPolicy } from './mealPeriodPolicies/california.js';
+import { montanaPolicy } from './mealPeriodPolicies/montana.js';
+import { nevadaPolicy } from './mealPeriodPolicies/nevada.js';
+import { oregonPolicy } from './mealPeriodPolicies/oregon.js';
+import { washingtonPolicy } from './mealPeriodPolicies/washington.js';
+import { coloradoPolicy } from './mealPeriodPolicies/colorado.js';
+import { idahoPolicy } from './mealPeriodPolicies/idaho.js';
+import { wyomingPolicy } from './mealPeriodPolicies/wyoming.js';
 
 const mealPeriodPolicies = {
   California: californiaPolicy,
