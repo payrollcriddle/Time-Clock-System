ea9d152e7b01b7d12bd3a81443fe339776cac78e
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-28 21:19:58 -0700

    Update adminDashboard.js

--- dashboards/adminDashboard.js
+++ dashboards/adminDashboard.js
@@ -1,8 +1,8 @@
-import { getUser, logout } from '../auth.js';
+import { getUser, logout } from '../../auth.js';
 import { addEmployee, updateEmployee, deleteEmployee, getEmployees } from '../management/employeeManagement.js';
 import { addActivityType, deleteActivityType, getActivityTypes } from '../management/activityTypeManagement.js';
 import { addJob, deleteJob, getJobs } from '../management/jobManagement.js';
-import { getTimecard, approveTimecardEntry, rejectTimecardEntry } from '../timecard.js';
+import { getTimecard, approveTimecardEntry, rejectTimecardEntry } from '../../timecard.js';
 
 // Function to render admin dashboard
 export function renderAdminDashboard() {
