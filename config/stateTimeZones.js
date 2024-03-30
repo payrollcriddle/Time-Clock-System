0df96918d9ca9ddfd1fd4d164b187629b6013061
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-29 20:49:59 -0700

    Create stateTimeZones.js

---
+++ config/stateTimeZones.js
@@ -0,0 +1,12 @@
+// config/stateTimeZones.js
+
+export const stateTimeZones = {
+  California: -7, // Pacific Time (PT)
+  Colorado: -6, // Mountain Time (MT)
+  Idaho: -6, // Mountain Time (MT)
+  Montana: -6, // Mountain Time (MT)
+  Nevada: -7, // Pacific Time (PT)
+  Oregon: -7, // Pacific Time (PT)
+  Washington: -7, // Pacific Time (PT)
+  Wyoming: -6, // Mountain Time (MT)
+};
