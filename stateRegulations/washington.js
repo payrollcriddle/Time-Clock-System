32afc3aa8b767ca5d34ba5ca0a8a89a8db42b3ff
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-30 08:45:59 -0700

    Update washington.js

--- stateRegulations/washington.js
+++ stateRegulations/washington.js
@@ -1,1 +1,20 @@
-export { oregonRegulations as washingtonRegulations } from './oregon.js';
+export function washingtonRegulations(weeklyHours) {
+  let regularHours = Math.min(40, weeklyHours);
+  let overtimeHours = 0;
+
+  if (weeklyHours > 40) {
+    overtimeHours = weeklyHours - 40;
+  }
+
+  return regularHours;
+}
+
+export function washingtonOvertimeRegulations(weeklyHours) {
+  let overtimeHours = 0;
+
+  if (weeklyHours > 40) {
+    overtimeHours = weeklyHours - 40;
+  }
+
+  return overtimeHours;
+}
