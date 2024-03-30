9475e14296c037b27d848235eb99cad7c24752cc
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-30 08:45:08 -0700

    Update montana.js

--- stateRegulations/montana.js
+++ stateRegulations/montana.js
@@ -1,1 +1,20 @@
-export { oregonRegulations as montanaRegulations } from './oregon.js';
+export function montanaRegulations(weeklyHours) {
+  let regularHours = Math.min(40, weeklyHours);
+  let overtimeHours = 0;
+
+  if (weeklyHours > 40) {
+    overtimeHours = weeklyHours - 40;
+  }
+
+  return regularHours;
+}
+
+export function montanaOvertimeRegulations(weeklyHours) {
+  let overtimeHours = 0;
+
+  if (weeklyHours > 40) {
+    overtimeHours = weeklyHours - 40;
+  }
+
+  return overtimeHours;
+}
