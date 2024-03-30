159d46ec8d27e2d00159f94e16516b14c0b9b79b
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-30 08:44:57 -0700

    Update idaho.js

--- stateRegulations/idaho.js
+++ stateRegulations/idaho.js
@@ -1,1 +1,20 @@
-export { wyomingRegulations as idahoRegulations } from './wyoming.js';
+export function idahoRegulations(weeklyHours) {
+  let regularHours = Math.min(40, weeklyHours);
+  let overtimeHours = 0;
+
+  if (weeklyHours > 40) {
+    overtimeHours = weeklyHours - 40;
+  }
+
+  return regularHours;
+}
+
+export function idahoOvertimeRegulations(weeklyHours) {
+  let overtimeHours = 0;
+
+  if (weeklyHours > 40) {
+    overtimeHours = weeklyHours - 40;
+  }
+
+  return overtimeHours;
+}
