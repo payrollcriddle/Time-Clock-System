3061dd80b615971d859e25d6763fbe3e02bc5955
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-29 19:46:11 -0700

    Update stateConfig.js

--- stateConfig.js
+++ stateConfig.js
@@ -1,36 +1,50 @@
+// stateConfig.js
+
 const stateConfig = {
-  California: {
-    regulationFile: 'california.js',
-    policyFile: 'california.js'
-  },
-  Colorado: {
-    regulationFile: 'colorado.js',
-    policyFile: 'colorado.js'
-  },
-  Idaho: {
-    regulationFile: 'idaho.js',
-    policyFile: 'idaho.js'
-  },
-  Montana: {
-    regulationFile: 'montana.js',
-    policyFile: 'montana.js'
-  },
-  Nevada: {
-    regulationFile: 'nevada.js',
-    policyFile: 'nevada.js'
-  },
-  Oregon: {
-    regulationFile: 'oregon.js',
-    policyFile: 'oregon.js'
-  },
-  Washington: {
-    regulationFile: 'washington.js',
-    policyFile: 'washington.js'
-  },
-  Wyoming: {
-    regulationFile: 'wyoming.js',
-    policyFile: 'wyoming.js'
-  }
+    California: {
+        regulationFile: 'california.js',
+        policyFile: 'california.js'
+    },
+    Colorado: {
+        regulationFile: 'colorado.js',
+        policyFile: 'colorado.js'
+    },
+    Idaho: {
+        regulationFile: 'idaho.js',
+        policyFile: 'idaho.js'
+    },
+    Montana: {
+        regulationFile: 'montana.js',
+        policyFile: 'montana.js'
+    },
+    Nevada: {
+        regulationFile: 'nevada.js',
+        policyFile: 'nevada.js'
+    },
+    Oregon: {
+        regulationFile: 'oregon.js',
+        policyFile: 'oregon.js'
+    },
+    Washington: {
+        regulationFile: 'washington.js',
+        policyFile: 'washington.js'
+    },
+    Wyoming: {
+        regulationFile: 'wyoming.js',
+        policyFile: 'wyoming.js'
+    }
 };
+
+export function getStates() {
+    return Object.keys(stateConfig);
+}
+
+export function getRegulationFile(state) {
+    return stateConfig[state]?.regulationFile;
+}
+
+export function getPolicyFile(state) {
+    return stateConfig[state]?.policyFile;
+}
 
 export default stateConfig;
