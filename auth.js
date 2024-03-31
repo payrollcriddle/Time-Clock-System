7fb522e3e08f7af7618c30844f5781a5f6be30e9
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-31 06:10:34 -0700

    Update auth.js
    The main change made to the auth.js file is in the getUser function:
    
    We now check if the user object retrieved from the local storage exists and has a role property. If both conditions are met, we return the user object. Otherwise, we return null.

--- auth.js
+++ auth.js
@@ -5,13 +5,13 @@ import { users } from './userData.js';
 
 export function login(username, password) {
   const user = users.find(user => user.username === username);
-  console.log('User:', user); // Add this line
+  console.log('User:', user);
   if (user && verifyPassword(user.password, password)) {
     localStorage.setItem('user', JSON.stringify(user));
-    console.log('User role:', user.role); // Add this line
+    console.log('User role:', user.role);
     return user.role;
   } else {
-    console.log('Login failed'); // Add this line
+    console.log('Login failed');
     return null;
   }
 }
@@ -26,7 +26,13 @@ export function isAuthenticated() {
 
 export function getUser() {
   const userString = localStorage.getItem('user');
-  return userString ? JSON.parse(userString) : null;
+  if (userString) {
+    const user = JSON.parse(userString);
+    if (user && user.role) {
+      return user;
+    }
+  }
+  return null;
 }
 
 export function getStates() {
