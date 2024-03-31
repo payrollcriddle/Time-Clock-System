d68652981c7e196adbaf56c872ddfffda6b40ee9
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-03-30 19:27:49 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,35 +4,35 @@ import { getStates as fetchStates } from './stateConfig.js';
 import { users } from './userData.js';
 
 export function login(username, password) {
-    const user = users.find(user => user.username === username);
-    if (user && verifyPassword(user.password, password)) {
-        localStorage.setItem('user', JSON.stringify(user));
-        return user.role;
-    } else {
-        return null;
-    }
+  const user = users.find(user => user.username === username);
+  if (user && verifyPassword(user.password, password)) {
+    localStorage.setItem('user', JSON.stringify(user));
+    return user.role;
+  } else {
+    return null;
+  }
 }
 
 export function logout() {
-    localStorage.removeItem('user');
+  localStorage.removeItem('user');
 }
 
 export function isAuthenticated() {
-    return localStorage.getItem('user') !== null;
+  return localStorage.getItem('user') !== null;
 }
 
 export function getUser() {
-    const userString = localStorage.getItem('user');
-    return userString ? JSON.parse(userString) : null;
+  const userString = localStorage.getItem('user');
+  return userString ? JSON.parse(userString) : null;
 }
 
 export function getStates() {
-    return fetchStates(); // Use the renamed function fetchStates
+  return fetchStates();
 }
 
 // Function to verify password (placeholder for demonstration purposes)
 function verifyPassword(storedPassword, enteredPassword) {
-    // Replace this with your own password verification logic
-    // For example, you can compare the hashed and salted passwords
-    return storedPassword === enteredPassword;
+  // Replace this with your own password verification logic
+  // For example, you can compare the hashed and salted passwords
+  return storedPassword === enteredPassword;
 }
