ed02f7379bd1b090f47d56110ca4e88e1df67e3e
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-04-02 10:06:05 -0700

    Update oregon.js

--- mealPeriodPolicies/oregon.js
+++ mealPeriodPolicies/oregon.js
@@ -1,5 +1,4 @@
-import { calculateTotalHours } from './mealPeriodPolicyUtils.js';
-
+// mealPeriodPolicies/oregon.js
 export const oregonPolicy = {
   mealPeriodDuration: 30,
   mealPeriodThresholds: [
@@ -8,22 +7,4 @@ export const oregonPolicy = {
     { threshold: 22, mealPeriods: 3 },
   ],
   waivable: false,
-
-  shouldPromptForMealPeriodWaiver: function(dailyTimecard) {
-    const totalHours = calculateTotalHours(dailyTimecard);
-
-    for (let i = 0; i < this.mealPeriodThresholds.length; i++) {
-      const { threshold, mealPeriods } = this.mealPeriodThresholds[i];
-
-      if (totalHours > threshold) {
-        const takenMealPeriodCount = dailyTimecard.filter(entry => entry.activityTypeId === 'meal').length;
-
-        if (takenMealPeriodCount < mealPeriods) {
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
 };
