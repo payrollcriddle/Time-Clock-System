e15e26fdb144634469a579a51dfbb2a705484aaa
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-04-02 10:06:29 -0700

    Update wyoming.js

--- mealPeriodPolicies/wyoming.js
+++ mealPeriodPolicies/wyoming.js
@@ -1,22 +1,7 @@
-import { calculateTotalHours } from './mealPeriodPolicyUtils.js';
-
+// mealPeriodPolicies/wyoming.js
 export const wyomingPolicy = {
   mealPeriodDuration: 30,
   mealPeriodThreshold: 5,
   overtimeMealPeriodThreshold: 3,
   waivable: true,
-
-  shouldPromptForMealPeriodWaiver: function(dailyTimecard) {
-    const totalHours = calculateTotalHours(dailyTimecard);
-
-    if (totalHours > this.mealPeriodThreshold) {
-      const hasTakenMealPeriod = dailyTimecard.some(entry => entry.activityTypeId === 'meal');
-
-      if (!hasTakenMealPeriod) {
-        return true;
-      }
-    }
-
-    return false;
-  }
 };
