e48d826c02c411f34c3de4c29c14ca48c5862808
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-04-02 07:45:29 -0700

    Update montana.js

--- mealPeriodPolicies/montana.js
+++ mealPeriodPolicies/montana.js
@@ -1,4 +1,8 @@
 export const montanaPolicy = {
   mealPeriodDuration: 30,
   waivable: false,
+
+  shouldPromptForMealPeriodWaiver: function(dailyTimecard) {
+    return false; // Meal periods are not waivable in Montana
+  }
 };
