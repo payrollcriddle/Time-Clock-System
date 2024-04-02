9aa46e4fc58b7d4f349a4d3ecbaecd297e0716a6
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-04-02 07:44:58 -0700

    Update colorado.js

--- mealPeriodPolicies/colorado.js
+++ mealPeriodPolicies/colorado.js
@@ -4,4 +4,22 @@ export const coloradoPolicy = {
   uninterrupted: true,
   dutyFree: true,
   canBeUnpaid: true,
+
+  shouldPromptForMealPeriodWaiver: function(dailyTimecard) {
+    const totalHours = calculateTotalHours(dailyTimecard);
+    
+    if (totalHours > this.mealPeriodThreshold) {
+      const hasTakenMealPeriod = dailyTimecard.some(entry => entry.activityTypeId === 'meal');
+      
+      if (!hasTakenMealPeriod) {
+        return true;
+      }
+    }
+    
+    return false;
+  }
 };
+
+function calculateTotalHours(dailyTimecard) {
+  // ... (same as in california.js)
+}
