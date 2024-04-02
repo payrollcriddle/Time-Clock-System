373a59996b680540dc7f594b196ac60e6c2fe00f
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-04-02 10:04:50 -0700

    Update california.js

--- mealPeriodPolicies/california.js
+++ mealPeriodPolicies/california.js
@@ -1,29 +1,8 @@
-import { calculateTotalHours } from './mealPeriodPolicyUtils.js';
-
+// mealPeriodPolicies/california.js
 export const californiaPolicy = {
   mealPeriodDuration: 30,
   mealPeriodThreshold: 5,
   secondMealPeriodThreshold: 10,
   maxHoursWithoutSecondMealPeriod: 12,
   waivable: true,
-
-  shouldPromptForMealPeriodWaiver: function(dailyTimecard) {
-    const totalHours = calculateTotalHours(dailyTimecard);
-
-    if (totalHours > this.mealPeriodThreshold && totalHours <= this.secondMealPeriodThreshold) {
-      const hasTakenMealPeriod = dailyTimecard.some(entry => entry.activityTypeId === 'meal');
-
-      if (!hasTakenMealPeriod) {
-        return true;
-      }
-    } else if (totalHours > this.secondMealPeriodThreshold && totalHours <= this.maxHoursWithoutSecondMealPeriod) {
-      const mealPeriodCount = dailyTimecard.filter(entry => entry.activityTypeId === 'meal').length;
-
-      if (mealPeriodCount < 2) {
-        return true;
-      }
-    }
-
-    return false;
-  }
 };
