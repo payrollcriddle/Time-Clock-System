58abc6b262259ae9e40a9066649458edb0ae6fa1
payrollcriddle <164409353+payrollcriddle@users.noreply.github.com>
2024-04-02 08:07:19 -0700

    Update oregon.js

--- mealPeriodPolicies/oregon.js
+++ mealPeriodPolicies/oregon.js
@@ -1,3 +1,5 @@
+import { calculateTotalHours } from './mealPeriodPolicyUtils.js';
+
 export const oregonPolicy = {
   mealPeriodDuration: 30,
   mealPeriodThresholds: [
@@ -9,37 +11,19 @@ export const oregonPolicy = {
 
   shouldPromptForMealPeriodWaiver: function(dailyTimecard) {
     const totalHours = calculateTotalHours(dailyTimecard);
-    
+
     for (let i = 0; i < this.mealPeriodThresholds.length; i++) {
       const { threshold, mealPeriods } = this.mealPeriodThresholds[i];
-      
+
       if (totalHours > threshold) {
         const takenMealPeriodCount = dailyTimecard.filter(entry => entry.activityTypeId === 'meal').length;
-        
+
         if (takenMealPeriodCount < mealPeriods) {
           return true;
         }
       }
     }
-    
+
     return false;
   }
 };
-
-function calculateTotalHours(dailyTimecard) {
-  let totalHours = 0;
-  
-  for (let i = 0; i < dailyTimecard.length; i++) {
-    const entry = dailyTimecard[i];
-    
-    if (entry.activityTypeId !== 'meal') {
-      const startTime = new Date(entry.startTime);
-      const endTime = entry.endTime ? new Date(entry.endTime) : new Date(); // Use current time if no end time
-      
-      const duration = (endTime - startTime) / 3600000; // Convert milliseconds to hours
-      totalHours += duration;
-    }
-  }
-  
-  return totalHours;
-}
